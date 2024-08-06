151d3b3e9e1f28a8ad08d62b9f4737aae749273c
AllaHanol <156720109+AllaHanol@users.noreply.github.com>
2024-08-06 14:11:25 +0200

    counter

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,21 @@
+// import React from "react";
+// function Counter() {
+    
+//     const [count, setCount] = React.useState(0); 
+//         const onClickPlus = () => {
+//             setCount(count +1)
+//         }
+//         const onClickMinus = () => {
+//             setCount(count - 1)
+        
+//         return ( <div>
+//                 <h2>Counter</h2>
+//                 <h1>{count}</h1>
+//                 <button onClick={onClickMinus}>Minus</button>
+//                 <button onClick={onClickPlus}>Plus</button>
+//             </div>
+//         )
+//     };
+// }
+
+// export default Counter;
